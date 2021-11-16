7f4bf3510b2f3d6de022fe2c2e99ebfdf2d68bfd
kami <ceacosta@escuelasproa.edu.ar>
2021-11-15 21:01:30 -0300

    json flores

--- server.js
+++ server.js
@@ -3,6 +3,19 @@ const app= express();
 const port= 6000;
 const bodyParser= require('body-parser');
 var suma=0;
+
+[
+    {
+    "nombreFlor":"jazmin","precio":"150", "color":"blanca"
+    } ,
+    {
+     "nombreFlor":"margarita","precio":"120","color":"blanca"
+    },
+    {
+        "nombreFlor":"lavanda","precio":"180","color":"violeta"
+    }
+    ]
+
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
